d3ddc01b2091824b95dfa7638dadcee9fda45827
LAPTOP-83JT1FO4\lenovo <Fahira0505@gmail.com>
2021-12-01 12:36:46 +0800

    www

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,10 +9,12 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-<<<<<<< HEAD
+
     Nurul Fahira
-=======
+
     Pao pao vs limbung engku ajha
->>>>>>> 2e76bbbaa409db892187fbbceadce20832a6ca61
+
+    Nurul Fahira R
+    aku sehat
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
